540f838322645901c2bc822950a61a83da38937b
Marco Origlia <marco.origlia@santannapisa.it>
2018-10-22 18:03:57 +0200

    First Test With Corvus

---
+++ CoDASync/CorvusManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO.Ports;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace CoDASync
+{
+    class CorvusManager : SerialPort
+    {
+
+        public CorvusManager(string portName, int baudRate) : base(portName, baudRate)
+        {
+        }
+
+        protected void setOpen(){
+           if(!IsOpen){
+               Open();
+           }
+        }
+
+        public bool rmove(float a1, float a2, float a3){
+            setOpen();
+
+            string txData = Convert.ToString(a1) + " " + Convert.ToString(a2) + " " + Convert.ToString(a3);
+            txData += " rmove";
+
+            WriteLine(txData);
+
+            return true;
+        }
+    }
+}
---
+++ CoDASync/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CoDASync
+{
+    class MainClass
+    {
+        public static void Main(string[] args){
+            CorvusManager cm = new CorvusManager("COM6", 57600);
+            cm.rmove(-6, -5, -2);
+            Thread.Sleep(5000);
+            cm.rmove(6, 5, 2);
+        }
+    }
+}
